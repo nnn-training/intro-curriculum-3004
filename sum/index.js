75ce49a1db41f31de7c71a060b9dd4b91efbe530
Takahiro Ido <juve.taka@gmail.com>
2021-05-09 14:31:43 +0900

    yarnの利用法メモ追加

--- sum/index.js
+++ sum/index.js
@@ -15,7 +15,7 @@ function add(numbers) {
  * 数値の配列を受け取って、その要素の積を返す関数
  */
  function multi(numbers) {
-  let result = 0;
+  let result = 1;
   for (const num of numbers) {
     result = result * num;
   }
@@ -26,3 +26,10 @@ module.exports = {
   add: add,
   multi: multi
 };
+
+// - 自作ライブラリの作成方法：
+//   - ディレクトリを作り（＝作成するパッケージの名前）、yarn initを実行
+//   - .jsファイルを作成、その中に関数定義＋module.exportsでオブジェクトのプロパティ（メソッド）: 関数名と記載
+// - 自作ライブラリの利用方法：
+//   - 利用したいディレクトリでyarn init
+//   - Yarn add パッケージの相対パスでnode_modulesディレクトリに自作パッケージがインストールされる
