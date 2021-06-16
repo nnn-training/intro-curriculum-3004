853d52039ac9e23d7f6a7b1e390c4686127e8141
t.kawabata <t.kawabata20@gmail.com>
2021-06-16 16:20:09 +0900

    multi関数作成

--- sum/index.js
+++ sum/index.js
@@ -10,5 +10,15 @@ function add(numbers) {
   }
   return result;
 }
+/**
+ * 数値の配列を受け取って、その要素の掛け合わせて返す関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result *= num;
+  }
+  return result;
+}
 
-module.exports = { add };
+module.exports = { add, multi };
