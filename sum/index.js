2b803e9ddf7154cb011645af79968348be63fbce
Kys <ym185215@gmail.com>
2021-07-28 13:07:59 +0900

    モジュールを正常に掛け算できるように修正â

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,18 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+/**
+ * 
+ * @param {number} numbers 
+ * @returns 掛け算の結果
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add : add};
+module.exports = { multi : multi }
