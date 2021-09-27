bf03b57cf2503caf0a1d95c24036726cb413a4ed
Takashi Nishimura <saishuheki.nisshing@gmail.com>
2021-09-28 00:02:16 +0900

    配列の整数をかけ合わせる関数を追加

--- sum-app/app.js
+++ sum-app/app.js
@@ -1,3 +1,8 @@
 'use strict';
+
+// sumパッケージを読み込む
 const s = require('sum');
+
+// 読み込んだsumパッケージのメソッドであるaddメソッドとmultiメソッドを使用する
 console.log(s.add([1, 2, 3, 4]));
+console.log(s.multi([1, 2, 3, 4]));
