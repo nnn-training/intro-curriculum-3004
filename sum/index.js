1feddfabeef7a017034d609e30c4c87c8ebc4c52
Shoma SUZUKI <suzu.shouma@gmail.com>
2021-10-05 23:31:25 +0900

    積の関数を追加

--- sum/index.js
+++ sum/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * 数値の配列を受け取って、その要素の合計を返す関数
+ * 数値の配列を受け取って、その合計を返す関数
  */
-function add(numbers) {
+ function add(numbers) {
   let result = 0;
   for (const num of numbers) {
     result = result + num;
@@ -11,4 +11,15 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+/**
+ * 数値の配列を受け取って、その要素の積を返す関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
