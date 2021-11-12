0eec75f4166d7c6096444061f39dad362ee92993
E233-3639 <ryoogisoe233@gmail.com>
2021-11-12 10:39:42 +0900

    配列で渡された 整数を全て掛けあわせて返す関数 を追加実装

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,20 @@ function add(numbers) {
   return result;
 }
 
+/**
+ * 数値の配列を受け取って、その要素を全て掛け合わす関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers)
+  {
+    result *= num;
+  }
+  return result;
+}
+
 module.exports = { add };
+module.exports = {
+  add,
+  multi: multi 
+};
