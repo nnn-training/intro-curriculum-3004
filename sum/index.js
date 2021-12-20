fd6b7665bf3d33279be8baf9d8a9ebf38d8ec7ee
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-12-20 21:12:24 +0900

    足し算関数を残す

--- sum/index.js
+++ sum/index.js
@@ -3,6 +3,17 @@
 /**
  * 数値の配列を受け取って、その要素の合計を返す関数
  */
+ function add(numbers) {
+  let result = 0;
+  for (const num of numbers) {
+    result = result + num;
+  }
+  return result;
+}
+
+/** 
+ * 数値の配列を受け取って、その要素の積を返す関数
+ */
 function multi(numbers) {
   let result = 1;
   for (const num of numbers) {
@@ -11,4 +22,4 @@ function multi(numbers) {
   return result;
 }
 
-module.exports = { multi };
+module.exports = { add,multi };
