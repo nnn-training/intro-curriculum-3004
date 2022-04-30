d256431f38594ad30dbe0a49bbc71cb6541e66e1
kustona <103849008+kustona@users.noreply.github.com>
2022-04-30 12:35:19 +0900

    multiを追加

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,16 @@ function add(numbers) {
   return result;
 }
 
+/**
+ * 数値の配列を受け取って、その要素の総積を返す関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result *= num;
+  }
+  return result;
+}
+
 module.exports = { add };
+module.exports = { multi };
