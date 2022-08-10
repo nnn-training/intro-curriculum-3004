be31e210e307e69085bc0e95a9eb5519230c682a
Manaita <gaoh0329@gmail.com>
2022-08-10 19:14:46 +0900

    24の出力

--- sum/index.js
+++ sum/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-/**
- * 数値の配列を受け取って、その要素の合計を返す関数
- */
+
 function add(numbers) {
   let result = 0;
   for (const num of numbers) {
@@ -11,4 +9,12 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
