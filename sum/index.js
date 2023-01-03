183d0fade87967180b714ac213b30ecfa214cedd
kofun0117 <ocean_pv@yahoo.co.jp>
2023-01-03 15:31:01 +0900

    multi関数をパッケージに追加

--- sum/index.js
+++ sum/index.js
@@ -10,5 +10,18 @@ function add(numbers) {
   }
   return result;
 }
+/**
+ * 数値の配列を受け取って、その要素をすべて掛けあわせて返す関数
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+module.exports = 
+{ add,
+  multi
+};
 
-module.exports = { add };
