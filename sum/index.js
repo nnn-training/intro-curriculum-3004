67708e0b225cb748c43c24f8dd6d8f3413269dc2
shuei yoshida <20shu0929@gmail.com>
2023-11-20 15:27:03 +0900

    乗法の出力

--- sum/index.js
+++ sum/index.js
@@ -3,12 +3,21 @@
 /**
  * 数値の配列を受け取って、その要素の合計を返す関数
  */
-function add(numbers) {
+
+function add(numbers){
   let result = 0;
-  for (const num of numbers) {
-    result = result + num;
+  for (const num of numbers){
+      result = result + num;
   }
   return result;
 }
 
-module.exports = { add };
+function multi(numbers){
+  let result = 1;
+  for (const num of numbers){
+      result = result * num
+  }
+  return result;
+}
+
+module.exports = {add,multi};
