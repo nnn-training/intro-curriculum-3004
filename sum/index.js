ca01df1f2da9da11f71c6fb157e3f9f31d0c220b
hiiro-deduction <202639784+hiiro-deduction@users.noreply.github.com>
2025-07-13 11:30:37 +0900

    渡された整数をすべて掛け合わせて返す関数を追加

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,16 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+/**
+ * 配列で渡された整数をすべて掛け合わせて返す関数
+ * @param {Array<number>} numbers 
+ */
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
