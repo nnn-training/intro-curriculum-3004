1c3dfb57fcf2459ec59670649cc22268b8cf102e
ueta-shunsuke <206701210+chazuke-ueta@users.noreply.github.com>
2026-02-10 11:38:44 +0900

    「配列で渡された 整数をすべて掛けあわせて返す関数」を追加

--- sum/index.js
+++ sum/index.js
@@ -2,13 +2,17 @@
 
 /**
  * 数値の配列を受け取って、その要素の合計を返す関数
+ * 「配列で渡された整数をすべて掛けあわせて返す関数」を追加
+ * バージョン実験
  */
-function add(numbers) {
-  let result = 0;
+
+
+function multi(numbers) {
+  let result = 1;
   for (const num of numbers) {
-    result = result + num;
+    result = result * num;
   }
   return result;
 }
 
-module.exports = { add };
+module.exports = { multi };
