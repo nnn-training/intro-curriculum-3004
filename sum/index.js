6194218ba3e724a93d1585e5d6c9b3fe44f4edc6
SakuInVR <sakuinvr@gmail.com>
2026-02-10 12:02:21 +0900

    multi関数を実装

--- sum/index.js
+++ sum/index.js
@@ -3,6 +3,7 @@
 /**
  * 数値の配列を受け取って、その要素の合計を返す関数
  */
+
 function add(numbers) {
   let result = 0;
   for (const num of numbers) {
@@ -11,4 +12,12 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+function multi(numbers) {
+  let result = 1;
+  for (const num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
