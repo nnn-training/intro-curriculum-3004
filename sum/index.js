acb0bd390b6872a9df4e1ffb91c318ae939b2e01
gai1010 <tvxlion03@gmail.com>
2026-04-12 23:27:11 +0900

    multi関数を実装

--- sum/index.js
+++ sum/index.js
@@ -11,4 +11,19 @@ function add(numbers) {
   return result;
 }
 
-module.exports = { add };
+/**
+ * 数値の配列を受け取って、その要素を掛け合わせた数を掛け合わす
+ */
+
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result *= num;
+  }
+  return result;
+}
+
+module.exports = {
+  add,
+  multi 
+};
